a77144a3b9fd10ba5e514b8160483c965b72a052
TheoRoudilValentin <theo.roudil@ens-paris-saclay.fr>
2021-08-19 15:03:02 +0200

    Push final Théo

--- Theo/Exploration/comparaison.py
+++ Theo/Exploration/comparaison.py
@@ -5,23 +5,26 @@ from joblib import Parallel,delayed
 import functools
 import operator
 import torch
+
 import fats
 from tqdm import tqdm
 import sys
 os.chdir("C:\\Users\\theo.roudil-valentin\\Documents\\Resume\\MLSUM")
 
-index_2=pickle.load(open('index_2.pickle','rb'))
-trace=pickle.load(open('test/trace_test.pickle','rb'))
-score=pickle.load(open('test/score.pickle','rb'))
-score=[score[i] for i in index_2]
-assert len(score)==len(trace)
-s=Parallel(5)(delayed(fats.make_new_paragraphes)(i,j) for i,j in zip(score,trace))
-s=functools.reduce(operator.iconcat,s,[])
+# index_2=pickle.load(open('index_2.pickle','rb'))
+# trace=pickle.load(open('test/trace_test.pickle','rb'))
+# score=pickle.load(open('test/score.pickle','rb'))
+# score=[score[i] for i in index_2]
+# assert len(score)==len(trace)
+# s=Parallel(5)(delayed(fats.make_new_paragraphes)(i,j) for i,j in zip(score,trace))
+# s=functools.reduce(operator.iconcat,s,[])
 P=pickle.load(open('test/Paragraphes_.pickle','rb'))
-assert len(P)==len(s)
-s=Parallel(5)(delayed(fats.make_new_sortie)(i) for i in s)
-#%%
+# assert len(P)==len(s)
+# s=Parallel(5)(delayed(fats.make_new_sortie)(i) for i in s)
+
 # pickle.dump(s,open('score_final_test.pickle','wb'))
+s=pickle.load(open('score_final_test.pickle','rb'))
+
 # #%%
 # k=4
 # N=[f for f in os.listdir('test') if (f.split('.')[-1]=='pickle') and ('sortie' in f)]
@@ -76,10 +79,117 @@ for i in range(len(s)):
 nul=torch.stack(ouais).isnan().to(torch.long).topk(torch.stack(ouais).isnan().to(torch.long).sum())[1]
 nul
 # %%
-fats.F1_score().false_positive_mean(sortie[0],s[0])
+Prec={}
+N=['L3_sortie.pickle','RS_sortie.pickle','TRW_sortie.pickle','TRB_sortie_finale.pickle',
+'BSR_sortie.pickle','Multi_sortie_finale.pickle','Simple_sortie_finale.pickle',
+'SMHA_sortie.pickle','Net_sortie_4.pickle']
+for n in N:
+    try:
+        sortie=pickle.load(open('test/'+n,'rb'))
+        sortie=Parallel(5)(delayed(fats.make_new_sortie)(i,j) for i,j in zip(s,sortie))
+
+        a=Parallel(5)(delayed(fats.F1_score().true_positive_mean)(i,j) for i,j in zip(sortie,s))
+        b=Parallel(5)(delayed(fats.F1_score().false_negative_mean)(i,j) for i,j in zip(sortie,s))
+        c=Parallel(5)(delayed(fats.F1_score().false_positive_mean)(i,j) for i,j in zip(sortie,s))
+        d=Parallel(5)(delayed(fats.F1_score().precision)(i,j) for i,j in zip(sortie,s))
+        e=Parallel(5)(delayed(fats.F1_score().recall)(i,j) for i,j in zip(sortie,s))
+        f=Parallel(5)(delayed(fats.F1_score)(i,j) for i,j in zip(sortie,s))
+        a=torch.stack(a).mean()
+        b=torch.stack(b).mean()
+        c=torch.stack(c).mean()
+        d=torch.stack(c).mean()
+        e=torch.stack(e).mean()
+        f=torch.stack(f).mean()
+        print(n.split('.')[0],[a,b,c,d,e,f])
+        Prec[n.split('.')[0]]=[a,b,c,d,e,f]
+
+    except:
+        print(n.split('.')[0])
+        Prec[n.split('.')[0]]=[]
 
 # %%
-import gensim
-W2V=gensim.models.Word2Vec.load("W2V_all.model")
-TR=fats.TextRank(tok_path='MLSUM_tokenizer.model')
-TR.make_resume(P[0],type='word2vec',modele=W2V,get_score_only=True,weights=True,alpha=0.2)
+Prec={}
+#%%
+n='BSR_sortie.pickle'
+S=[]
+sortie_multi=pickle.load(open('test/'+n,'rb'))
+# sortie_multi=[torch.tensor(i).to(torch.long) for i in sortie_multi]
+erreur=[]
+for i in range(len(s)):
+    try:
+        S.append(fats.make_new_sortie(s[i],sortie_multi[i]))
+    except:
+        print("Unexpected error:", sys.exc_info())
+        # break
+        print(i)
+        erreur.append(i)
+
+try:
+    if erreur[0]==201:
+        s_prime=s[:201]+s[202:]
+except:
+    print('ok')
+#%%
+s_prime=s
+a=Parallel(5)(delayed(fats.F1_score().true_positive_mean)(i,j) for i,j in zip(S,s_prime))
+a=torch.stack(a).mean()
+print(a)
+c=Parallel(5)(delayed(fats.F1_score().false_positive_mean)(i,j) for i,j in zip(S,s_prime))
+c=torch.stack(c).mean()
+print(c)
+b=Parallel(5)(delayed(fats.F1_score().false_negative_mean)(i,j) for i,j in zip(S,s_prime))
+b=torch.stack(b).mean()
+print(b)
+
+
+
+d=Parallel(5)(delayed(fats.F1_score().precision)(i,j) for i,j in zip(S,s_prime))
+d=torch.stack(d).mean()
+print(d)
+
+e=Parallel(5)(delayed(fats.F1_score().recall)(i,j) for i,j in zip(S,s_prime))
+e=torch.stack(e).mean()
+print(e)
+
+f=Parallel(5)(delayed(fats.F1_score())(i,j) for i,j in zip(S,s_prime))
+f=torch.stack(f).mean()
+print(f)
+
+print(n.split('.')[0],[a,c,b,d,e,f])
+Prec[n.split('.')[0]]=[a,c,b,d,e,f]
+
+#%%
+n='SMHA_sortie.pickle'
+#'TRW_sortie.pickle'
+#'Net_sortie_4.pickle'
+sortie=pickle.load(open('test/'+n,'rb'))
+sortie=[torch.tensor(i).to(torch.long) for i in sortie]
+sortie=Parallel(5)(delayed(fats.make_new_sortie)(i,j) for i,j in zip(s,sortie))
+
+a=Parallel(5)(delayed(fats.F1_score().true_positive_mean)(i,j) for i,j in zip(sortie,s))
+a=torch.stack(a).mean()
+print(a)
+c=Parallel(5)(delayed(fats.F1_score().false_positive_mean)(i,j) for i,j in zip(sortie,s))
+c=torch.stack(c).mean()
+print(c)
+b=Parallel(5)(delayed(fats.F1_score().false_negative_mean)(i,j) for i,j in zip(sortie,s))
+b=torch.stack(b).mean()
+print(b)
+
+
+
+d=Parallel(5)(delayed(fats.F1_score().precision)(i,j) for i,j in zip(sortie,s))
+d=torch.stack(d).mean()
+print(d)
+
+e=Parallel(5)(delayed(fats.F1_score().recall)(i,j) for i,j in zip(sortie,s))
+e=torch.stack(e).mean()
+print(e)
+
+f=Parallel(5)(delayed(fats.F1_score())(i,j) for i,j in zip(sortie,s))
+f=torch.stack(f).mean()
+print(f)
+
+print(n.split('.')[0],[a,c,b,d,e,f])
+Prec[n.split('.')[0]]=[a,c,b,d,e,f]
+# %%
